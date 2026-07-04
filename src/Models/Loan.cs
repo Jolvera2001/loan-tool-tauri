b1d57c6794d4d2095e523bf47cc6346d0711b1b7
Jolvera2001 <jolvera6@stedwards.edu>
2026-07-03 20:31:35 -0500

    added a table with viewmodel

--- src/Models/Loan.cs
+++ src/Models/Loan.cs
@@ -1,10 +1,21 @@
 public class Loan
 {
     public Guid Id { get; set; }
-    public DateTime date_created { get; set; }
-    public DateTime date_updated { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime DateUpdated { get; set; }
     public decimal Principal { get; set; }
     public decimal Rate { get; set; }
     public decimal MonthlyPayment { get; set; }
     public int Months { get; set; }
+
+    public Loan(decimal principal, decimal rate, decimal monthlyPayment, int months)
+    {
+        Id = Guid.NewGuid();
+        DateCreated = DateTime.UtcNow;
+        DateUpdated = DateTime.UtcNow;
+        Principal = principal;
+        Rate = rate;
+        MonthlyPayment = monthlyPayment;
+        Months = months;
+    }
 }
--- src/ViewModels/MainViewModel.cs
+++ src/ViewModels/MainViewModel.cs
@@ -1,13 +1,32 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using DecimalMath;
 
-public partial class MainViewModel: ObservableObject
+public partial class MainViewModel : ObservableObject
 {
     [ObservableProperty] private decimal _principal;
     [ObservableProperty] private decimal _rate;
     [ObservableProperty] private decimal _months;
-    [ObservableProperty] private decimal _monthlyPayment;
+    [ObservableProperty] private decimal? _monthlyPayment;
+
+    private ObservableCollection<Loan> _loans = [];
+
+    public ReadOnlyObservableCollection<Loan> Loans { get; }
+
+    public MainViewModel()
+    {
+        Loans = new(_loans);
+    }
+
+    [RelayCommand]
+    public async Task AddNewLoan()
+    {
+        if (MonthlyPayment is null) return;
+        var monthlyPayment = MonthlyPayment.Value;
+        var newLoan = new Loan(Principal, Rate, monthlyPayment, (int)Months);
+        _loans.Add(newLoan);
+    }
 
     [RelayCommand]
     public async Task CalculateMonthlyPayment()
